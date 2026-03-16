258b3cd6f5232fe4a647f6cf46288231ba99cad3
Axel <a.letourneurch@gmail.com>
2026-03-16 23:47:04 +0100

    fix: minimal self-contained middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware({
+    // A list of all locales that are supported
+    locales: ["en", "fr"],
 
-
+    // Used when no locale matches
+    defaultLocale: "fr",
+});
 
 export const config = {
     // Match only internationalized pathnames
     matcher: ["/", "/(en|fr)/:path*"],
 };
-
-
